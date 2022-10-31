e6a711a7a2c3506cfac497179dccfebb125e670f
Kirk Rohani <kirk.rohani@gmail.com>
2022-10-30 23:48:25 -0500

    removed expo stuff

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.textStyle}>
+      <Text>Tesla Command v2</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  textStyle: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
