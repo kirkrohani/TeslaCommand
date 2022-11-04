4ee6c40fb6383816d00cd1783cca3d92785be7c8
Kirk Rohani <kirk.rohani@gmail.com>
2022-11-03 21:39:33 -0500

    Added some views to set a basic layout for home screen

--- App.js
+++ App.js
@@ -1,19 +1,98 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
-    <View style={styles.textStyle}>
-      <Text>Tesla Command v2</Text>
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.topNavBar}>
+        <View style={styles.menuSection}>
+          <Text>Menu Section</Text>
+        </View>
+        <View style={styles.userProfileSection}>
+          <Text>User Profile Section</Text>
+        </View>
+      </View>
+      <View style={styles.topSection}>
+        <View style={styles.vehicleDataSection}>
+          <Text>Vehicle Data </Text>
+        </View>
+        <View style={styles.vehicleImageSection}>
+          <Text>Vehicle Image </Text>
+        </View>
+        <View style={styles.vehicleStatsSection}>
+          <Text>Vehicle Stats </Text>
+        </View>
+      </View>
+      <View style={styles.importantMessagesSection}>
+        <Text>Important Messages </Text>
+      </View>
+      <View style={styles.quickControlsSection}>
+        <Text>Quick Controls </Text>
+      </View>
+    </SafeAreaView> 
   );
 }
 
 const styles = StyleSheet.create({
-  textStyle: {
-    flex: 1,
+  topNavBar: {
+    flex: .05,
+    flexDirection: "row",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "space-between",
+    backgroundColor: '#0000FF',
+    borderWidth: 3
+  },
+  menuSection: {
+    backgroundColor: "#89CFF0",
+    height: "100%",
+    borderWidth: 1,
+    flex: .5
+  },
+  userProfileSection: {
+    backgroundColor: "#89CFF0",
+    height: "100%",
+    borderWidth: 1,
+    flex: .5,
+    alignItems: "flex-end"
+  },
+  topSection: {
+    flex: .35,
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#00FFFF",
+    justifyContent: "space-evenly",
+    borderWidth: 3
+  },
+  vehicleDataSection: {
+    borderWidth: 1,
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    flex: .33
+  },
+  vehicleImageSection: {
+    borderWidth: 1,
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    flex: .33
+  },
+  vehicleStatsSection: {
+    borderWidth: 1,
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    flex: .33
+  },
+  importantMessagesSection: {
+    flex: .05,
+    borderWidth: 3,
+    borderColor: "green"
+  },
+  quickControlsSection: {
+    flex: .55,
+    borderWidth: 3,
+    borderColor: "yellow"
   }
 });
